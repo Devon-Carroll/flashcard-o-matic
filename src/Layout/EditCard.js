9def297515b5d69c4fb7d90c3e7ceaa6a9584793
Devon <41218582+Devon-Carroll@users.noreply.github.com>
2023-08-24 12:08:22 -0400

    Update EditCard.js

--- src/Layout/EditCard.js
+++ src/Layout/EditCard.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { useHistory, useParams, Link } from "react-router-dom";
-import { readCard, updateCard } from "../utils/api";
+import { readCard, updateCard, readDeck } from "../utils/api";
+import Form from "./Form";
 
 function EditCard() {
   const history = useHistory();
-  const { cardId } = useParams();
+  const { cardId, deckId } = useParams();
   const [card, setCard] = useState(null);
   const [cardFront, setCardFront] = useState("");
   const [cardBack, setCardBack] = useState("");
+  const [deck, setDeck] = useState(null);
+
+
+  useEffect(() => {
+    async function loadDeck() {
+        try {
+            const deckData = await readDeck(deckId);
+            setDeck(deckData);
+        } catch (error) {
+            console.error("Error loading deck:", error);
+        }
+    }
+    loadDeck();
+}, [deckId]);
 
   useEffect(() => {
     async function loadCard() {
@@ -16,6 +31,8 @@ function EditCard() {
         setCard(cardData);
         setCardFront(cardData.front);
         setCardBack(cardData.back);
+
+        
       } catch (error) {
         console.error("Error loading card:", error);
       }
@@ -30,7 +47,7 @@ function EditCard() {
             id: cardId,
             front: cardFront,
             back: cardBack,
-            deckId: card.deckId,
+            deckId: deckId,
         });
         console.log("Card updated:", updatedCard);
         history.push(`/decks/${card.deckId}`);
@@ -39,7 +56,7 @@ function EditCard() {
     }
   };
 
-  const handleCancel = () => {
+  const handleDone = () => {
     history.push(`/decks/${card.deckId}`);
   }
 
@@ -47,44 +64,33 @@ function EditCard() {
     return <p>Loading...</p>;
   }
 
+  const fields = [
+    {
+      name: "front",
+      label: "Front",
+      type: "textarea",
+      placeholder: "Front side of card",
+      value: cardFront,
+      onChange: (event) => setCardFront(event.target.value),
+    },
+    {
+      name: "back",
+      label: "Back",
+      type: "textarea",
+      placeholder: "Back side of card",
+      value: cardBack,
+      onChange: (event) => setCardBack(event.target.value),
+    },
+  ];
+
   return (
     <div>
       <nav>
-          <Link to="/">Home</Link> / Edit Card 
-        </nav>
-        <h1>Edit Card</h1>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="front">Front</label>
-                <br />
-                <textarea
-                id="front"
-                type="textarea"
-                name="front"
-                placeholder="Front of the card"
-                value={cardFront}
-                onChange={(event) => setCardFront(event.target.value)}
-                ></textarea>
-            </div>
-            <div>
-                <label htmlFor="back">Back</label>
-                <br />
-                <textarea
-                id="back"
-                type="textarea"
-                name="back"
-                placeholder="Back of the card"
-                value={cardBack}
-                onChange={(event) => setCardBack(event.target.value)}
-                ></textarea>
-            </div>
-            <div>
-                <button type="submit">Submit</button>
-                <button type="button" onClick={handleCancel}>
-                    Cancel
-                </button>
-            </div>
-        </form>
+        <Link to="/">Home</Link> / <Link to={`/decks/${deckId}`}>{deck.name}</Link> /{" "}
+        <span>Edit Card {cardId}</span>
+      </nav>
+      <h2>{deck.name}: Edit Card</h2>
+      <Form fields={fields} handleSubmit={handleSubmit} handleDone={handleDone} />
     </div>
   );
 }
